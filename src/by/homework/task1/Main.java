5859d3fe4c429ffa8766b2aa69a35a2e64ed9ef1
Veronika <veronika.slesarchyk@gmail.com>
2025-09-14 11:02:09 +0300

    First task

---
+++ src/by/homework/task1/Main.java
@@ -0,0 +1,9 @@
+package by.homework.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Фамилия: Слесарчик");
+        System.out.println("Дата и время получения задания: 10.09.2025 21:03");
+    }
+}
+
