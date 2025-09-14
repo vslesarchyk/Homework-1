01042d87a0a7c8d8460a99aa2b8e808637bace89
Veronika <veronika.slesarchyk@gmail.com>
2025-09-14 10:46:06 +0300

    First commit.Homework 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Фамилия: Слесарчик");
+        System.out.println("Дата и время получения задания: 10.09.2025 21:03");
+    }
+}
